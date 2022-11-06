c6dc6aa2b35a2a559c264e2967107d83f5fc31aa
teaquest <56929496+teaquest@users.noreply.github.com>
2022-11-06 16:03:52 -0500

    added docstring and one example

---
+++ conditional_expressions.py
@@ -0,0 +1,12 @@
+"""
+Practice for conditional expressions
+
+Resources:
+    https://realpython.com/python-conditional-statements/#conditional-expressions-pythons-ternary-operator
+
+"""
+
+
+raining = False
+
+print("Let's go to the ", 'beach' if not raining else 'library')
